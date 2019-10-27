3e1cb80b801e7ad508de379c49976b3ee321baed
Dilber Gür <d.gur@gmx.de>
2019-10-27 20:33:08 +0100

    added test file

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,3 +1,17 @@
+const app = require('../app');
+const request = require('supertest');
+
+// `describe` is used for test components
+describe('GET /', () => {
+    
+    // `it` is for individual tests
+    it('responds with 200', async () => {
+        await request(app)
+            .get('/')
+            .expect(200); // If the status code is not 200, this test will fail
+    });
+})
+
 describe('Sample Test', () => {
     it('should test that true === true', () => {
       expect(true).toBe(true)
