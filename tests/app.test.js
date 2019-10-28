3ca61289780a6d1bb570f8cd07ce6d45a211b530
Dilber Gür <d.gur@gmx.de>
2019-10-28 15:46:37 +0100

    changed test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,14 +1,13 @@
 const app = require('../app');
 const request = require('supertest');
 
-// `describe` is used for test components
 describe('GET /', () => {
     
     // `it` is for individual tests
     it('responds with 200', async () => {
         await request(app)
             .get('/')
-            .expect(200); // If the status code is not 200, this test will fail
+            .expect(200); 
     });
 })
 
@@ -16,4 +15,4 @@ describe('Sample Test', () => {
     it('should test that true === true', () => {
       expect(true).toBe(true)
     })
-  })
+  });
